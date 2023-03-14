b87af126288b78df8e2652704659174f660e8aa2
bkmi <12955549+bkmi@users.noreply.github.com>
2023-03-14 15:22:54 +0100

    removed support for methods which require the graph to be retained

--- sbi/utils/sbiutils.py
+++ sbi/utils/sbiutils.py
@@ -858,7 +858,7 @@ def gradient_ascent(
             optimizer.zero_grad()
             probs = potential_fn(theta_transform.inv(optimize_inits)).squeeze()
             loss = -probs.sum()
-            loss.backward(retain_graph=True)
+            loss.backward()
             optimizer.step()
 
             with torch.no_grad():
@@ -892,18 +892,10 @@ def gradient_ascent(
                 max_val = best_log_prob_overall
 
             iter_ += 1
-            
-        # free memory
-        loss.backward()
-        optimizer.zero_grad()
 
     except KeyboardInterrupt:
         interruption = f"Optimization was interrupted after {iter_} iterations. "
         print(interruption + interruption_note)
-
-        # free memory
-        loss.backward()
-        optimizer.zero_grad()
 
         return argmax_, max_val  # type: ignore
 
